d683f663de8482bc955243095d9739abee8155ef
Printlab3d <custom3d@wp.pl>
2025-12-17 18:22:28 +0100

    Update App.jsx

--- gemini/src/App.jsx
+++ gemini/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import Pages from "@/pages/index.jsx"
+// DODAJEMY IMPORT
+import { Toaster } from "@/components/ui/toaster"
 
 function App() {
   return (
     <>
       <Pages />
+      {/* DODAJEMY TOASTER TUTAJ - TYLKO RAZ */}
+      <Toaster />
     </>
   )
 }
