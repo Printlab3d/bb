ff30fc15b882bce8c2f84d89f69697d2f71cdc57
Printlab3d <custom3d@wp.pl>
2025-12-18 20:32:23 +0100

    Update App.jsx

--- gemini/src/App.jsx
+++ gemini/src/App.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import './App.css';
 import Pages from "@/pages/index.jsx";
-// Importujemy Toaster
+// IMPORTUJEMY TOASTER
 import { Toaster } from "@/components/ui/toaster";
 
 function App() {
   return (
-    // Używamy React.Fragment (<> ... </>)
     <>
-      {/* Główna zawartość strony */}
       <Pages />
       
-      {/* Toaster jest TUTAJ i TYLKO TUTAJ.
-        Dzięki temu powiadomienia będą wyświetlane raz, na wierzchu całej aplikacji.
-      */}
+      {/* TOASTER JEST TYLKO TUTAJ - POWIADOMIENIA BĘDĄ POJEDYNCZE */}
       <Toaster />
     </>
   );
