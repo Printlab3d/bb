bcd04d350abdefabc2ae85ddff7a597a5159072e
Printlab3d <custom3d@wp.pl>
2026-01-04 20:56:13 +0100

    Update index.jsx

--- gemini/src/pages/index.jsx
+++ gemini/src/pages/index.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// Layout jest folder wyżej (w src), więc musimy wyjść ".."
-import Layout from "../Layout"; 
+
+// ZMIANA TUTAJ: Używamy @/Layout zamiast ../Layout
+// @ wskazuje bezpośrednio na folder src, więc na 100% znajdzie plik.
+import Layout from "@/Layout"; 
 
 // Importy stron z tego samego folderu (pages)
 import Home from "./Home";
 import Moto from "./Moto";
 import CustomKeychains from "./CustomKeychains";
 import Okazje from "./Okazje";
-import ProductDetails from "./ProductDetails"; // lub ProductPage, zależnie jak nazwałeś plik
+import ProductDetails from "./ProductDetails"; 
 import Cart from "./Cart";
-import Checkout from "./Checkout"; // Jeśli używasz
+// import Checkout from "./Checkout"; // Jeśli używasz, odkomentuj
 
-// --- NOWE STRONY (Tego brakowało) ---
+// --- NOWE STRONY (Regulaminy) ---
 import Regulamin from "./Regulamin";
 import Zwroty from "./Zwroty";
 import PolitykaPrywatnosci from "./PolitykaPrywatnosci";
@@ -29,9 +31,10 @@ const Pages = () => {
         
         <Route path="/product/:id" element={<Layout currentPageName="Produkt"><ProductDetails /></Layout>} />
         <Route path="/Cart" element={<Layout currentPageName="Koszyk"><Cart /></Layout>} />
+        
         {/* <Route path="/Checkout" element={<Layout currentPageName="Zamowienie"><Checkout /></Layout>} /> */}
 
-        {/* --- TUTAJ DODAJEMY REGULAMIN --- */}
+        {/* --- STOPKA --- */}
         <Route path="/Regulamin" element={<Layout currentPageName="Regulamin"><Regulamin /></Layout>} />
         <Route path="/Zwroty" element={<Layout currentPageName="Zwroty"><Zwroty /></Layout>} />
         <Route path="/PolitykaPrywatnosci" element={<Layout currentPageName="Polityka"><PolitykaPrywatnosci /></Layout>} />
