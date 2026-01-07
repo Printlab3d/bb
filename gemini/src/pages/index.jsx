ff0e9020cd22dac5f4c7060861899fae939d5a5a
Printlab3d <custom3d@wp.pl>
2026-01-07 20:53:05 +0100

    Update index.jsx

--- gemini/src/pages/index.jsx
+++ gemini/src/pages/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// NAPRAWA: Kropka (.) oznacza "szukaj w tym samym folderze" (src/pages)
 import Layout from "./Layout.jsx"; 
 
 import Home from "./Home";
@@ -10,7 +9,9 @@ import CustomKeychains from "./CustomKeychains";
 import Okazje from "./Okazje";
 import ProductDetails from "./ProductDetails"; 
 import Cart from "./Cart";
-// import Checkout from "./Checkout"; 
+
+// NOWY IMPORT - CHECKOUT
+import Checkout from "./Checkout"; 
 
 // Stopka
 import Regulamin from "./Regulamin";
@@ -29,9 +30,10 @@ const Pages = () => {
         
         <Route path="/product/:id" element={<Layout currentPageName="Produkt"><ProductDetails /></Layout>} />
         <Route path="/Cart" element={<Layout currentPageName="Koszyk"><Cart /></Layout>} />
-        
-        {/* <Route path="/Checkout" element={<Layout currentPageName="Zamowienie"><Checkout /></Layout>} /> */}
 
+        {/* NOWA TRASA - CHECKOUT */}
+        <Route path="/Checkout" element={<Layout currentPageName="Zamowienie"><Checkout /></Layout>} />
+        
         <Route path="/Regulamin" element={<Layout currentPageName="Regulamin"><Regulamin /></Layout>} />
         <Route path="/Zwroty" element={<Layout currentPageName="Zwroty"><Zwroty /></Layout>} />
         <Route path="/PolitykaPrywatnosci" element={<Layout currentPageName="Polityka"><PolitykaPrywatnosci /></Layout>} />
